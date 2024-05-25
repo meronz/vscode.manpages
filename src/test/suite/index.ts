f3821ae8e99019218fa1a2505bb36f3cc8cc0c2e
Salvatore Merone <sal.merone95@gmail.com>
2024-05-25 07:52:06 +0200

    update packages

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
     // Create the mocha test
@@ -11,12 +11,8 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
-
+    return new Promise((c, e) =>
+        glob('**/**.test.js', { cwd: testsRoot }).then(files => {
             // Add files to the test suite
             files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
@@ -32,6 +28,5 @@ export function run(): Promise<void> {
             } catch (err) {
                 e(err);
             }
-        });
-    });
+        }));
 }
